d3ba5e8d7e6de44e0609afabf2cf9f49ac070ebe
Jampa Pranav Teja <pranavtejajampa@gmail.com>
2025-10-17 14:55:59 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'LoanGuard AI',
+  description: 'Bank loan default perdiction system for dav PBL',
 }
 
 export default function RootLayout({
